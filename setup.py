844c04ac15b39b76a325f56948a876e053c1edc6
Roland Dobai <roland.dobai@espressif.com>
2025-01-09 14:55:02 +0100

    Bump the version to 1.1.0

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setuptools.setup(
     description=SHORT_DESCRIPTION,
     long_description_content_type='text/markdown',
     license=LICENSE,
-    version='1.0.3',
+    version='1.1.0',
     author=AUTHOR,
     maintainer=MAINTAINER,
     author_email=EMAIL,
